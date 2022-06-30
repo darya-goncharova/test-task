dbec78a67c6563bb7c4c3d6d57da8098b3125f12
darya goncharova <darya.goncharova23@gmail.com>
2022-06-30 17:21:20 +0300

    feat: finished

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
 
-//  /*Burger*/
+ /*Burger*/
 
 
-//  const burgerItem = document.querySelector('.burger');
-//  const menu = document.querySelector('.header-nav');
-//  const section = document.querySelector('.shadow_wrap');
-//  const shelterLogo = document.querySelector('.header-logo');
-//  const burgerLogo = document.querySelector('.header-logo_burger');
+ const burgerItem = document.querySelector('.burger');
+ const menu = document.querySelector('.header-nav');
+
+
  
-  
- 
-//  function toggleMenu() {
-//    burgerItem.classList.toggle('hamburger__menu_open');
-//    }
-//    burgerItem.addEventListener('click', toggleMenu);
+ function toggleMenu() {
+   burgerItem.classList.toggle('hamburger__menu_open');
+   }
+   burgerItem.addEventListener('click', toggleMenu);
  
  
-//  (function () {
+ (function () {
  
-//  burgerItem.addEventListener('click', () => {
-//    document.body.classList.toggle('_lock');
-//    menu.classList.toggle('header-nav_open');
-//    section.classList.toggle('shadow_wrap_open');
-//    shelterLogo.classList.toggle('header-logo_open');
-//    burgerLogo.classList.toggle('header-logo_burger_open');
-//  });
-//  }());
+ burgerItem.addEventListener('click', () => {
+   document.body.classList.toggle('_lock');
+   menu.classList.toggle('header-nav_open');
+ });
+ }());
  
-//  menu.addEventListener("click", (event) => {
-//    if(event.target){
-//      menu.classList.remove("header-nav_open");
-//      burgerItem.classList.remove("hamburger__menu_open");
-//      document.body.classList.remove('_lock');
-//      section.classList.remove('section_open');
-//      shelterLogo.classList.remove('header-logo_open');
-//    burgerLogo.classList.remove('header-logo_burger_open');
-//   }
-//  });
+ menu.addEventListener("click", (event) => {
+   if(event.target){
+     menu.classList.remove("header-nav_open");
+     burgerItem.classList.remove("hamburger__menu_open");
+     document.body.classList.remove('_lock');
+     section.classList.remove('section_open');
+  }
+ });
 
 
 
